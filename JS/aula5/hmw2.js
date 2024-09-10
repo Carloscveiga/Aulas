3e3c1db8e352af0893f852dd5da342f947f403e4
Peres <eueue@DESKTOP-O8GM0RL>
2024-09-10 18:59:07 +0100

    added homework2

--- JS/aula5/hmw2.js
+++ JS/aula5/hmw2.js
@@ -49,24 +49,99 @@
 // Highest: 20
 // Lowest: 1
 
-let numbers = [4, 7, 10, 12, 1, 8, 20, 16];
-let sum = 0;
-let highest = numbers[0];
-let lowest = numbers[0];
 
+// *********************************************************************************************** //
 
-for ( let i = 0; i < numbers.length; i++) {
-     sum += numbers[i]; 
-     avg = sum / numbers.length;
-     if (numbers[i] > highest){
-        highest = numbers[i];
-     }
-     if (numbers[i] < lowest){
-        lowest = numbers[i];
-     }
+let numbers1 = [4, 7, 10, 12, 1, 8, 20, 16];
+
+function sumArray(numbers) {
+   let sum = 0;
+   for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i]; 
+   }
+   return sum;
 }
+
+function avgArray(numbers) {
+   let sum = sumArray(numbers); 
+   return sum / numbers.length;
+}
+
+function highestArray(numbers) {
+   let highest = numbers[0]; 
+   for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > highest) {
+         highest = numbers[i];
+      }
+   }
+   return highest;
+}
+
+function lowestArray(numbers) {
+   let lowest = numbers[0]; 
+   for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] < lowest) {
+         lowest = numbers[i];
+      }
+   }
+   return lowest;
+}
+
+
+
+console.time("Execution Time"); // inicio do script
+
+let sum = sumArray(numbers1);
+let avg = avgArray(numbers1);
+let highest = highestArray(numbers1);
+let lowest = lowestArray(numbers1);
 
 console.log("Sum:", sum);
 console.log("Average:", avg);
 console.log("Highest:", highest);
 console.log("Lowest:", lowest);
+
+
+console.timeEnd("Execution Time"); // fim do script
+
+console.log('// *********************************************************************************************** //');
+
+
+ // *********************************************************************************************** //
+
+let numbers2 = [4, 7, 10, 12, 1, 8, 20, 16];
+
+function doItAll(numbers) {
+   let sum = 0;
+   let highest = numbers[0];
+   let lowest = numbers[0];
+   let avg = 0;
+
+   for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i]; 
+
+      if (numbers[i] > highest) {
+         highest = numbers[i];
+      }
+      if (numbers[i] < lowest) {
+         lowest = numbers[i];
+      }
+   }
+   avg = sum / numbers.length;
+
+   return { sum, avg, highest, lowest }; 
+}
+
+console.time("Execution Time"); // inicio do script
+
+let result = doItAll(numbers2);
+
+console.log("Sum:", result.sum);
+console.log("Average:", result.avg);
+console.log("Highest:", result.highest);
+console.log("Lowest:", result.lowest);
+
+console.timeEnd("Execution Time"); // fim do script
+
+
+
